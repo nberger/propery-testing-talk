c9b3fa67006219cc44cc944c344b94f2cc49fac7
Nicolas Berger <nicoberger@gmail.com>
2014-08-01 20:16:23 -0300

    Bug fix: Partition when any variant != 0

--- lib/products_sorter.rb
+++ lib/products_sorter.rb
@@ -7,7 +7,7 @@ class ProductsSorter
 
     gte_100 = first_partition[0]
     lt_100 = first_partition[1].partition do |product|
-      product.variants.all? {|v| v.stock.to_i != 0 }
+      product.variants.any? {|v| v.stock.to_i != 0 }
     end.flatten
 
     gte_100.concat(lt_100)
