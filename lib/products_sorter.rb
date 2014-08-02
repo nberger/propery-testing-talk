a8a689a18bfce6089c37d8795cd610031c54da51
Nicolas Berger <nicoberger@gmail.com>
2014-08-01 21:30:26 -0300

    Introduce regression in sort, prop testing finds it

--- lib/products_sorter.rb
+++ lib/products_sorter.rb
@@ -2,7 +2,7 @@ class ProductsSorter
 
   def sort(products)
     first_partition = products.partition do |product|
-      product.variants.any? {|v| v.stock.to_i >= 100 }
+      product.variants.any? {|v| v.stock.to_i > 100 }
     end
 
     gte_100 = first_partition[0]
